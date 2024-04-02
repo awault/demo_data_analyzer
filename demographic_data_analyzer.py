6c6c7c7f351da9cd8cfe40be2a75058cedb4313e
awault <91794118+awault@users.noreply.github.com>
2024-04-02 12:08:57 -0500

    Education Stats Completed

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -1,39 +1,78 @@
 import pandas as pd
 
+# Read data from file
 data = pd.read_csv('adult.data.csv')
 
+# How many of each race are represented in this dataset?
+r_c = data.value_counts('race')
+
+# What is the average age of men?
+avg_age_m = round(data[data['sex']=='Male']['age'].mean(),1)
+
+# What is the percentage of people who have a Bachelor's degree?
+ed_percent = round(data['education'].value_counts(normalize=True) * 100,1)
+bachelors = ed_percent['Bachelors']
+
+# What percentage of people with advanced education
+adv_degree = ['Bachelors','Masters','Doctorate']
+perc_adv_degree = round(ed_percent[adv_degree], 1)
+
+# What percentage of people without advanced education
+perc_no_adv_degree = 100 - perc_adv_degree
+
+# What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
+data['higher_ed'] = data['education'].isin(adv_degree)
+salary_ed_table = pd.crosstab(data['higher_ed'],data['salary'],normalize='index')*100
+adv_deg_rich = round(salary_ed_table.loc[True,'>50K'], 1)
+
+# What percentage of people without advanced education make more than 50K?
+no_adv_degree_rich = round(salary_ed_table.loc[False,'>50K'], 1)
+
+# What is the minimum number of hours a person works per week
+min_weekly_hours = min(data['hours-per-week'])
+
+# What percentage of the people who work the minimum number of hours per week have a salary of >50K?
+hr_per_week = data['hours-per-week'].value_counts().sort_index()
+min_hr_count = hr_per_week.iloc[0]
+
+data['min_hr_worked'] = data['hours-per-week']==1
+min_hrs_salary_table = pd.crosstab(data['min_hr_worked'],data['salary'],normalize='index') * 100
+min_hrs_wealthy = min_hrs_salary_table.loc[True,'>50K']
+
+
+
+###################################################################
 
 def calculate_demographic_data(print_data=True):
     # Read data from file
     df = data
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+    race_count = r_c
 
     # What is the average age of men?
-    average_age_men = None
+    average_age_men = avg_age_m
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    percentage_bachelors = bachelors
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
 
     # with and without `Bachelors`, `Masters`, or `Doctorate`
-    higher_education = None
-    lower_education = None
+    higher_education = perc_adv_degree
+    lower_education = perc_no_adv_degree
 
     # percentage with salary >50K
-    higher_education_rich = None
-    lower_education_rich = None
+    higher_education_rich = adv_deg_rich
+    lower_education_rich = no_adv_degree_rich
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
-    min_work_hours = None
+    min_work_hours = min_weekly_hours
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
-    num_min_workers = None
-
-    rich_percentage = None
+    num_min_workers = min_hr_count
+    rich_percentage = min_hrs_wealthy
 
     # What country has the highest percentage of people that earn >50K?
     highest_earning_country = None
